1d8fb77ee79b2ce46c4cff4262773abf2cdda724
msousa <msousa@sjtu.edu.cn>
2025-04-08 15:30:43 +0800

    Use std::format c++20

--- src/Operator.cpp
+++ src/Operator.cpp
@@ -1,28 +1,10 @@
 #include "Operator.h"
 
+#include <format>
+
 namespace libqm {
 std::string Operator::to_string() const {
-  const Type type_val = type();
-  const Spin spin_val = spin();
-  const ubyte value_val = value();
-
-  std::string result;
-  result.reserve(4096);
-
-  result += 'c';
-  if (type_val == Type::Creation) {
-    result += '+';
-  }
-  result += '(';
-  result += (spin_val == Spin::Up) ? "↑" : "↓";
-  result += ',';
-
-  char num_buffer[256];
-  int len = std::sprintf(num_buffer, "%u", value_val);
-  result.append(num_buffer, len);
-
-  result += ')';
-
-  return result;
+  return std::format("c{}({},{})", (type() == Type::Creation) ? "+" : "",
+                     (spin() == Spin::Up) ? "↑" : "↓", value());
 }
 }  // namespace libqm
