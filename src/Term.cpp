681305c3a59bcbfcaebd3a22c6f2e4379d4f2705
msousa <msousa@sjtu.edu.cn>
2025-04-08 15:54:02 +0800

    Make (most of) Term functions constexpr

--- src/Term.cpp
+++ src/Term.cpp
@@ -17,71 +17,6 @@ std::string Term::to_string() const {
   return result;
 }
 
-Term Term::adjoint() const noexcept {
-  Term result(std::conj(c));
-  for (size_t i = 0; i < size(); i++) {
-    result.operators.push_back(operators[size() - i - 1].adjoint());
-  }
-  return result;
-}
-
-Term& Term::operator*=(const Term& value) noexcept {
-  c *= value.c;
-  operators.append_range(value.operators.begin(), value.operators.end());
-  return *this;
-}
-
-Term& Term::operator*=(const Operator& value) noexcept {
-  operators.push_back(value);
-  return *this;
-}
-
-Term& Term::operator*=(const complex_type& value) noexcept {
-  c *= value;
-  return *this;
-}
-
-Term& Term::operator/=(const complex_type& value) noexcept {
-  c /= value;
-  return *this;
-}
-
-Term operator*(const Term& a, const Term& b) noexcept {
-  Term result(a);
-  result *= b;
-  return result;
-}
-
-Term operator*(const Term& a, const Operator& b) noexcept {
-  Term result(a);
-  result *= b;
-  return result;
-}
-
-Term operator*(const Term& a, const Term::complex_type& b) noexcept {
-  Term result(a);
-  result *= b;
-  return result;
-}
-
-Term operator/(const Term& a, const Term::complex_type& b) noexcept {
-  Term result(a);
-  result /= b;
-  return result;
-}
-
-Term operator*(const Term::complex_type& a, const Term& b) noexcept {
-  Term result(a);
-  result *= b;
-  return result;
-}
-
-Term operator*(const Operator& a, const Term& b) noexcept {
-  Term result(a);
-  result *= b;
-  return result;
-}
-
 bool is_diagonal(const Term::container_type& operators) {
   std::unordered_map<size_t, int> counts;
 
@@ -92,26 +27,5 @@ bool is_diagonal(const Term::container_type& operators) {
 
   return std::all_of(counts.begin(), counts.end(),
                      [](const auto& element) { return element.second == 0; });
-}
-
-Term creation(Operator::Spin spin, size_t orbital) noexcept {
-  return Term({Operator::creation(spin, orbital)});
-}
-
-Term annihilation(Operator::Spin spin, size_t orbital) noexcept {
-  return Term({Operator::annihilation(spin, orbital)});
-}
-
-Term one_body(Operator::Spin spin1, size_t orbital1, Operator::Spin spin2,
-              size_t orbital2) noexcept {
-  return Term({Operator::creation(spin1, orbital1), Operator::annihilation(spin2, orbital2)});
-}
-
-Term density(Operator::Spin spin, size_t orbital) noexcept {
-  return Term({Operator::creation(spin, orbital), Operator::annihilation(spin, orbital)});
-}
-
-Term density_density(Operator::Spin spin1, size_t i, Operator::Spin spin2, size_t j) noexcept {
-  return density(spin1, i) * density(spin2, j);
 }
 }  // namespace libqm
--- src/Term.h
+++ src/Term.h
@@ -11,62 +11,128 @@ constexpr size_t cache_line_size = 64;  // NOTE: might be different
 struct __attribute__((aligned(cache_line_size))) Term {
   using complex_type = std::complex<float>;
 
-  using container_type =
-      StaticVector<Operator, (cache_line_size - sizeof(complex_type)) / sizeof(Operator) - 1, uint8_t>;
+  static constexpr size_t static_vector_size =
+      (cache_line_size - sizeof(complex_type)) / sizeof(Operator) - 1;
+
+  using container_type = StaticVector<Operator, static_vector_size, uint8_t>;
 
   complex_type c{1.0f, 0.0f};
-  container_type operators;
+  container_type operators{};
 
-  Term() noexcept = default;
-  ~Term() noexcept = default;
+  constexpr Term() noexcept = default;
+  constexpr ~Term() noexcept = default;
 
-  Term(const Term& other) noexcept = default;
-  Term& operator=(const Term& other) noexcept = default;
-  Term(Term&& other) noexcept = default;
-  Term& operator=(Term&& other) noexcept = default;
+  constexpr Term(const Term& other) noexcept = default;
+  constexpr Term& operator=(const Term& other) noexcept = default;
+  constexpr Term(Term&& other) noexcept = default;
+  constexpr Term& operator=(Term&& other) noexcept = default;
 
-  explicit Term(const Operator& x) noexcept : operators({x}) {}
-  explicit Term(const complex_type& x) noexcept : c(x) {}
-  explicit Term(const container_type& ops) noexcept : operators(ops) {}
-  explicit Term(container_type&& ops) noexcept : operators(std::move(ops)) {}
-  explicit Term(const complex_type& x, const container_type& ops) noexcept : c(x), operators(ops) {}
-  explicit Term(const complex_type& x, container_type&& ops) noexcept
-      : c(x), operators(std::move(ops)) {}
-  explicit Term(const std::initializer_list<Operator>& ops) noexcept : operators(ops) {}
-  explicit Term(const complex_type& x, const std::initializer_list<Operator>& ops) noexcept
+  explicit constexpr Term(const Operator& x) noexcept : operators({x}) {}
+  explicit constexpr Term(const complex_type& x) noexcept : c(x) {}
+  explicit constexpr Term(const container_type& ops) noexcept : operators(ops) {}
+  explicit constexpr Term(container_type&& ops) noexcept : operators(std::move(ops)) {}
+  explicit constexpr Term(const complex_type& x, const container_type& ops) noexcept
       : c(x), operators(ops) {}
+  explicit constexpr Term(const complex_type& x, container_type&& ops) noexcept
+      : c(x), operators(std::move(ops)) {}
+  explicit constexpr Term(std::initializer_list<Operator> init) noexcept : operators(init) {}
+  explicit constexpr Term(const complex_type& x, std::initializer_list<Operator> init) noexcept
+      : c(x), operators(init) {}
 
-  size_t size() const noexcept { return operators.size(); }
+  constexpr size_t size() const noexcept { return operators.size(); }
 
   std::string to_string() const;
 
-  bool operator==(const Term& other) const {
+  constexpr bool operator==(const Term& other) const {
     return c == other.c && operators == other.operators;
   }
 
-  Term adjoint() const noexcept;
+  constexpr Term adjoint() const noexcept {
+    Term result(std::conj(c));
+    for (size_t i = 0; i < size(); i++) {
+      result.operators.push_back(operators[size() - i - 1].adjoint());
+    }
+    return result;
+  }
 
-  Term& operator*=(const complex_type& value) noexcept;
-  Term& operator*=(const Term& value) noexcept;
-  Term& operator*=(const Operator& value) noexcept;
-  Term& operator/=(const complex_type& value) noexcept;
+  constexpr Term& operator*=(const Term& value) noexcept {
+    c *= value.c;
+    operators.append_range(value.operators.begin(), value.operators.end());
+    return *this;
+  }
+
+  constexpr Term& operator*=(const Operator& value) noexcept {
+    operators.push_back(value);
+    return *this;
+  }
+
+  constexpr Term& operator*=(const complex_type& value) noexcept {
+    c *= value;
+    return *this;
+  }
+
+  constexpr Term& operator/=(const complex_type& value) noexcept {
+    c /= value;
+    return *this;
+  }
 };
-static_assert(sizeof(Term::container_type) == 56);
 static_assert(sizeof(Term) == 64, "Should fit a single cache line");
 
-Term operator*(const Term& a, const Term& b) noexcept;
-Term operator*(const Term& a, const Operator& b) noexcept;
-Term operator*(const Term& a, const Term::complex_type& b) noexcept;
-Term operator/(const Term& a, const Term::complex_type& b) noexcept;
-Term operator*(const Term::complex_type& a, const Term& b) noexcept;
-Term operator*(const Operator& a, const Term& b) noexcept;
+constexpr Term operator*(const Term& a, const Term& b) noexcept {
+  Term result(a);
+  result *= b;
+  return result;
+}
+
+constexpr Term operator*(const Term& a, const Operator& b) noexcept {
+  Term result(a);
+  result *= b;
+  return result;
+}
+
+constexpr Term operator*(const Term& a, const Term::complex_type& b) noexcept {
+  Term result(a);
+  result *= b;
+  return result;
+}
+
+constexpr Term operator/(const Term& a, const Term::complex_type& b) noexcept {
+  Term result(a);
+  result /= b;
+  return result;
+}
+
+constexpr Term operator*(const Term::complex_type& a, const Term& b) noexcept {
+  Term result(a);
+  result *= b;
+  return result;
+}
+
+constexpr Term operator*(const Operator& a, const Term& b) noexcept {
+  Term result(a);
+  result *= b;
+  return result;
+}
 
 bool is_diagonal(const Term::container_type& operators);
 
-Term creation(Operator::Spin spin, size_t orbital) noexcept;
-Term annihilation(Operator::Spin spin, size_t orbital) noexcept;
-Term one_body(Operator::Spin spin1, size_t orbital1, Operator::Spin spin2,
-              size_t orbital2) noexcept;
-Term density(Operator::Spin spin, size_t orbital) noexcept;
-Term density_density(Operator::Spin spin1, size_t i, Operator::Spin spin2, size_t j) noexcept;
+constexpr Term creation(Operator::Spin spin, size_t orbital) noexcept {
+  return Term({Operator::creation(spin, orbital)});
+}
+
+constexpr Term annihilation(Operator::Spin spin, size_t orbital) noexcept {
+  return Term({Operator::annihilation(spin, orbital)});
+}
+
+constexpr Term one_body(Operator::Spin s1, size_t o1, Operator::Spin s2, size_t o2) noexcept {
+  return Term({Operator::creation(s1, o1), Operator::annihilation(s2, o2)});
+}
+
+constexpr Term density(Operator::Spin s, size_t o) noexcept {
+  return Term({Operator::creation(s, o), Operator::annihilation(s, o)});
+}
+
+constexpr Term density_density(Operator::Spin s1, size_t i, Operator::Spin s2, size_t j) noexcept {
+  return density(s1, i) * density(s2, j);
+}
 }  // namespace libqm
